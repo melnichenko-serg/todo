718d00f089f68359065649e1ef79d01be5c247a7
melnichenko.s.a <melnichenko.s.a@gmail.com>
2017-04-04 10:22:34 +0300

    init

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::group(['middleware' => 'api', 'prefix' => 'v8'], function () {
+
+    Route::get('test', function () {
+        dd(database_path());
+        dd(Auth::guard('api')->user());
+    });
+
+});
